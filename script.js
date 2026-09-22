4a9c9902a55d0db0673ba458d13fbf72059f117d
Laanesh <laneplanetov@gmail.com>
2026-09-22 14:59:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const statusButton = document.querySelector('.status-button');
+const resetButton = document.querySelector('.reset-button');
+const statusText = document.querySelector('.status-text');
+const statusPanel = document.querySelector('.status-panel');
+
+// TODO: добавьте обработчики click по заданию ПЗ №5.
